e578df17e3b30f665b8c1bd1d98b46a80e4efdb2
iejMac <kilianmaciej6@gmail.com>
2024-06-21 19:18:55 +0200

    negexp relpos workign in jax

--- train.py
+++ train.py
@@ -6,12 +6,14 @@ from flax.training.train_state import TrainState
 
 from gpt_lw.data import get_dataset, sample_batch
 from gpt_lw.model_nanodo import DoConfig, TransformerDo
+from gpt_lw.loss import get_weighted_loss
 
 def train(
         dataset_path: str,
         n_steps: int,
         batch_size: int,
         seq_len: int,
+        loss_weighting: str = "unweighted",
         lr: float = 1e-3,
         seed: int = 42,
     ):
@@ -43,30 +45,25 @@ def train(
     optimizer = optax.adamw(lr)
     state = TrainState.create(apply_fn=model.apply, params=params['params'], tx=optimizer)
 
-    def loss_fn(params, x, y):
-        logits = state.apply_fn({'params': params}, x)
-        loss = losses.softmax_cross_entropy_with_integer_labels(logits, y)
-        return loss.mean()
+    loss_fn = get_weighted_loss(state, loss_weighting)
 
     for step in range(n_steps):
         data_key, batch_key = jax.random.split(data_key)
         batch = sample_batch(ds, batch_size, seq_len, batch_key)
-        x, y = batch[:, :-1], batch[:, 1:]
+        xt, xtp1 = batch[:, :-1], batch[:, 1:]
 
-        loss = loss_fn(state.params, x, y)
+        loss = loss_fn(state.params, xt, xtp1)
         print("loss: ", loss)
 
-        grads = jax.grad(loss_fn)(state.params, x, y)
+        grads = jax.grad(loss_fn)(state.params, xt, xtp1)
         state = state.apply_gradients(grads=grads)
-
-        print("batch: ", batch.shape)
-
 
 
 if __name__ == "__main__":
     train(
         dataset_path="cfg_dataset/simple4_100000.txt",
-        n_steps=100,
+        n_steps=10,
         batch_size=8,
         seq_len=5,
+        loss_weighting="negexp_relpos",
     )
