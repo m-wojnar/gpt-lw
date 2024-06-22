53481f50e82adf5f65b35a7d45ef099030fd27ea
iejMac <kilianmaciej6@gmail.com>
2024-06-22 14:11:52 +0200

    training (#2)
    * training
    
    * negexp relpos workign in jax
    
    ---------
    
    Co-authored-by: Maciek Kilian <maciej@Macieks-MacBook-Pro.local>

--- gpt_lw/loss.py
+++ gpt_lw/loss.py
@@ -1,25 +1,49 @@
 import optax
+import jax.numpy as jnp
+from jax import lax
+from optax import losses
 from flax import linen as nn
 
-from .nn import forward
+# TODO: needs benchmarking, can we make it faster?
+def compute_relative_positions(tokens, delim_token):
+    batch_size, seq_len = tokens.shape
+    relative_positions = jnp.full_like(tokens, fill_value=-seq_len)
+    
+    delim_mask = (tokens == delim_token) 
+    delim_mask = delim_mask.at[:, 0].set(True) # sometimes first pos is not delim
+    
+    positions = jnp.arange(seq_len).reshape(1, -1)
+    relative_positions = jnp.where(delim_mask, positions, relative_positions)
+
+    # Compute cumulative maximum along the sequence length for each batch
+    def cumulative_max_scan(carry, x):
+        carry = jnp.maximum(carry, x)
+        return carry, carry
+
+    last_pos = lax.scan(cumulative_max_scan, relative_positions[:, 0], relative_positions.T)[1].T
+    # Compute relative positions
+    relative_positions = positions - last_pos
+    return relative_positions
 
 
-def get_weight_fn(dataset):
-    def weight_fn(y):
-        return 1.0
+def get_weighted_loss(state, weighting):
+    if weighting == "unweighted":
+        def unweighted(x):
+            return 1.0
+        weight_fn = unweighted
+    elif weighting == "negexp_relpos":
+        def negexp_relpos(x):
+            relative_positions = compute_relative_positions(x, delim_token=0)
+            weights = (-jnp.exp(-relative_positions) + 1.0) * (27.0 / 26.0) + 1e-3
+            return weights
+        weight_fn = negexp_relpos
 
-    return weight_fn
+    def weighted_nt(params, xt, xtp1):
+        logits = state.apply_fn({'params': params}, xt)
+        token_loss = losses.softmax_cross_entropy_with_integer_labels(logits, xtp1)
+        weights = weight_fn(xt)
 
+        weighted_loss = token_loss * weights
+        return weighted_loss.mean()
 
-def eval_fn(x, y, weight_fn):
-    x, y = x.reshape(-1, x.shape[-1]), y.reshape(-1)
-    y = nn.one_hot(y, x.shape[-1])
-    loss = optax.sigmoid_binary_cross_entropy(x, y).reshape(x.shape[0], -1).sum(axis=-1).mean()
-    wloss = weight_fn(y) * loss
-    return wloss
-
-
-def loss_fn(params, state, key, x, y, model, weight_fn):
-    logits, state = forward(model, params, state, key, x)
-    loss = eval_fn(logits, y, weight_fn)
-    return loss, (state, loss)
+    return weighted_nt
--- train.py
+++ train.py
@@ -1,12 +1,20 @@
 import jax
+import jax.numpy as jnp
+import optax
+from optax import losses
+from flax.training.train_state import TrainState
+
 from gpt_lw.data import get_dataset, sample_batch
-from gpt_lw.gpt import GPT
+from gpt_lw.model_nanodo import DoConfig, TransformerDo
+from gpt_lw.loss import get_weighted_loss
 
 def train(
         dataset_path: str,
         n_steps: int,
         batch_size: int,
         seq_len: int,
+        loss_weighting: str = "unweighted",
+        lr: float = 1e-3,
         seed: int = 42,
     ):
 
@@ -21,22 +29,41 @@ def train(
     ds, tokenizer = get_dataset(dataset_path)
 
     # init model
-    # TODO
+    config = DoConfig(
+        D=16,
+        H=4,
+        L=64,
+        N=4,
+        V=tokenizer.vocab_size,
+        F=64 * 2,
+    )
+    model = TransformerDo(config)
+
+    inputs = jnp.ones((batch_size, seq_len), jnp.int32)
+    params = model.init(model_key, inputs)
+
+    optimizer = optax.adamw(lr)
+    state = TrainState.create(apply_fn=model.apply, params=params['params'], tx=optimizer)
+
+    loss_fn = get_weighted_loss(state, loss_weighting)
 
     for step in range(n_steps):
         data_key, batch_key = jax.random.split(data_key)
         batch = sample_batch(ds, batch_size, seq_len, batch_key)
+        xt, xtp1 = batch[:, :-1], batch[:, 1:]
 
-        print(step, batch)
+        loss = loss_fn(state.params, xt, xtp1)
+        print("loss: ", loss)
 
+        grads = jax.grad(loss_fn)(state.params, xt, xtp1)
+        state = state.apply_gradients(grads=grads)
 
 
 if __name__ == "__main__":
-    # TODO: get config
-    # TODO: pass into main
     train(
         dataset_path="cfg_dataset/simple4_100000.txt",
         n_steps=10,
-        batch_size=2,
+        batch_size=8,
         seq_len=5,
+        loss_weighting="negexp_relpos",
     )
