9510af2b61246db021f0cfd39af04f0cba67147a
iejMac <kilianmaciej6@gmail.com>
2024-06-22 21:46:37 +0200

    generate_dataset.py: create val split as well

--- generate_dataset.py
+++ generate_dataset.py
@@ -4,28 +4,42 @@ from cfg_dataset.cfg import CFG
 
 if __name__ == "__main__":
     cfg_name = "simple4"
-    n_samples = 100000
+    n_train_samples = 100000
+    n_val_samples = 1000
     delim_token = ","
-    dataset_file = f"cfg_dataset/{cfg_name}_{n_samples}.txt"
+    dataset_name = f"{cfg_name}_{n_train_samples}"
     log_stats = True
 
     cfg = CFG(rules_file=f"cfg_dataset/configs/{cfg_name}.cfg")
 
-    print(f"Generating dataset for CFG {cfg_name} with {n_samples} samples...")
-    samples = [cfg.sample() for _ in tqdm(range(n_samples))]
-    dataset_str = delim_token.join(samples)
+    print(f"Generating dataset for CFG {cfg_name} with {n_train_samples} samples...")
+    samples = [cfg.sample() for _ in tqdm(range(n_train_samples))]
+
+    unique_samples = list(set(samples))
+    print(f"Number of unique samples: {len(unique_samples)}")
+
+    val_samples = unique_samples[:n_val_samples]
+    train_samples = [s for s in samples if s not in val_samples]
+    print(f"Number of train samples: {len(train_samples)}")
+    print(f"Number of val samples: {len(val_samples)}")
+
+    train_dataset_str = "," + delim_token.join(train_samples) + ","
+    val_dataset_str = "," + delim_token.join(val_samples) + ","
 
     if log_stats:
-        avg_len = sum([len(s) for s in samples]) / n_samples
-        n_tokens = len(dataset_str)
-        vocab_size = len(set(dataset_str))
+        avg_len = sum([len(s) for s in train_samples]) / len(train_samples)
+        n_tokens = len(train_dataset_str)
+        vocab_size = len(set(train_dataset_str))
         dataset_size_gb = n_tokens * 1e-9 # 1 char = 1 byte
 
-        print(f"Dataset stats for CFG {cfg_name} with {n_samples} samples:")
+        print(f"Train dataset stats for CFG {cfg_name} with {len(train_samples)} samples:")
         print(f"Average length: {avg_len}")
         print(f"Number of tokens: {n_tokens}")
         print(f"Vocabulary size: {vocab_size}")
 
-    print(f"Writing dataset to file {dataset_file}")
-    with open(dataset_file, "w") as f:
-       f.write(dataset_str)
+    splits = [("train", train_dataset_str), ("val", val_dataset_str)]
+    for split_name, dataset_str in splits:
+        dataset_file = f"cfg_dataset/{dataset_name}_{split_name}.txt"
+        print(f"Writing {split_name} dataset to file {dataset_file}")
+        with open(dataset_file, "w") as f:
+            f.write(dataset_str)
