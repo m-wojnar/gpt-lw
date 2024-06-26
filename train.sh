076d2887d8155cdd0ed981394a3a108d595bcc39
iejMac <kilianmaciej6@gmail.com>
2024-06-26 16:16:05 +0200

    update scripts

--- train.sh
+++ train.sh
@@ -6,5 +6,5 @@ VENV_DIR="${BASE_DIR}/venv"
 cd ${BASE_DIR}
 source ${VENV_DIR}/bin/activate
 
-python train.py "$@" --loss_weighting negexp_relpos
-python train.py "$@" --loss_weighting unweighted
+python train.py "$@" --gpt_config configs/gpt/long_ctx.yaml --optimizer_config configs/optimizer/base.yaml --train_config configs/train/cfg3b.yaml --loss_weighting negexp_relpos --run_name cf3g3b_negexp_relpos
+python train.py "$@" --gpt_config configs/gpt/long_ctx.yaml --optimizer_config configs/optimizer/base.yaml --train_config configs/train/cfg3b.yaml --loss_weighting unweighted --run_name cf3g3b_unweighted
