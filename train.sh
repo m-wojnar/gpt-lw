17221f471726dd88a91c5f363116c177294beff2
iejMac <kilianmaciej6@gmail.com>
2024-06-29 00:30:49 +0200

    update script

--- train.sh
+++ train.sh
@@ -7,4 +7,7 @@ cd ${BASE_DIR}
 source ${VENV_DIR}/bin/activate
 
 # python train.py --gpt_config configs/gpt/long_ctx.yaml --optimizer_config configs/optimizer/base.yaml --train_config configs/train/base_txt.yaml --loss_weighting unweighted --run_name wiki_mini
-python train.py --gpt_config configs/gpt/long_ctx.yaml --optimizer_config configs/optimizer/base.yaml --train_config configs/train/base_txt.yaml --loss_weighting runs/wiki_mini/analysis/weights.npy --run_name wiki_mini_empirical_relpos_weighting
+python train.py --gpt_config configs/gpt/long_ctx.yaml --optimizer_config configs/optimizer/base.yaml --train_config configs/train/base_txt.yaml --loss_weighting runs/wiki_mini/analysis/weights_pow_smoothed.npy --run_name wiki_mini_empirical_relpos_weighting
+
+# stop pod
+runpodctl stop pod $RUNPOD_POD_ID
