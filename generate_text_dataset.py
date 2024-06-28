32439140e91cecd2133bb14b0e5d3880a3466620
iejMac <kilianmaciej6@gmail.com>
2024-06-28 17:32:08 +0200

    test sth

--- generate_text_dataset.py
+++ generate_text_dataset.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     wikipedia_dir = "text_dataset/wikipedia/"
     dataset_name = "wikipedia"
     shuffle = True
-    train_n_pages = 10000
+    train_n_pages = 100
     val_n_pages = 10
 
     parquet_files = glob.glob(os.path.join(wikipedia_dir, "*.parquet"))
