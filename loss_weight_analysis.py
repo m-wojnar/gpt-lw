bb34c2dfcb65a997766fda36bb49103073a37567
iejMac <kilianmaciej6@gmail.com>
2024-06-30 18:31:58 +0200

    code for analyzing loss weights

---
+++ loss_weight_analysis.py
@@ -0,0 +1,154 @@
+import jax
+import jax.numpy as jnp
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from gpt_lw.loss import compute_relative_positions
+from gpt_lw.data import TextTokenizer
+
+enc = TextTokenizer()
+
+
+# DELIM_TOKEN = enc.encode("\n")
+# DELIM_TOKEN = enc.encode("\n ")
+# DELIM_TOKEN = enc.encode(".")
+# DELIM_TOKEN = enc.encode(" that")
+# DELIM_TOKEN = enc.encode(" someone")
+# DELIM_TOKEN = enc.encode(" the")
+
+# DELIM_TOKEN = enc.encode("\n\n")
+DELIM_TOKEN = enc.encode("<|endoftext|>")
+
+assert len(DELIM_TOKEN) == 1, DELIM_TOKEN
+DELIM_TOKEN = DELIM_TOKEN[0]
+DELIM_TOKEN_STR = enc.decode(DELIM_TOKEN)
+print(f"delim token ({DELIM_TOKEN_STR}): ", DELIM_TOKEN)
+
+# N_RP = 511
+N_RP = 511 - 100
+SEQ_LEN = 512 - 1
+
+def compute_avg_loss_per_token():
+    # load Xs, Ys, and Ls
+    Xs = jnp.load("runs/tm_wiki_mini/analysis/Xs.npy")
+    Ys = jnp.load("runs/tm_wiki_mini/analysis/Ys.npy")
+    Ls = jnp.load("runs/tm_wiki_mini/analysis/Ls.npy")
+    print(f"Xs: {Xs.shape}, Ys: {Ys.shape}, Ls: {Ls.shape}")
+    
+    # cut off first half of sequence (axis=1)
+    Xs = Xs[:, 100:]
+    Ys = Ys[:, 100:]
+    Ls = Ls[:, 100:]
+
+    # check if DELIM_TOKEN in Xs
+    dt_mask = Xs == DELIM_TOKEN
+    dt_mask = jnp.any(dt_mask, axis=1)
+
+    # remove rows which don't have dt_mask == 1
+    Xs = Xs[dt_mask]
+
+    if Xs.shape[0] == 0:
+        print("DELIM_TOKEN not found in Xs")
+        return
+
+    n = Xs.shape[0]
+
+    Xs = Xs[:n]
+
+    RPs = compute_relative_positions(Xs, delim_token=DELIM_TOKEN)
+
+    # array for each relative position value (shape (N_RP,))
+    count = [0] * N_RP
+    loss_sum = [0] * N_RP
+
+    # compute how many tokens have each relative position and sum the loss for each position
+    for i in tqdm(range(Xs.shape[0])):
+        for j in range(Xs.shape[1]):
+            rp = RPs[i, j]
+            l = Ls[i, j]
+            count[rp] += 1
+            loss_sum[rp] += l.item()
+
+    # compute average
+    avg_loss = [loss_sum[i] / count[i] if count[i] != 0 else 0 for i in range(N_RP)]
+
+    # save as numpy array
+    # np.save("runs/wiki_mini/analysis/avg_loss.npy", np.array(avg_loss))
+
+    # matplotlib code to plot the loss for each relative position
+    plt.plot(avg_loss)
+    plt.title(DELIM_TOKEN_STR)
+    plt.show()
+
+def convert_to_pdist(avg_loss_per_token):
+    weights = avg_loss_per_token
+    weights = np.array(weights)
+
+    # normalize weights so its prob dist
+    weights = weights / np.sum(weights)
+    # invert so higher loss -> lower weight
+    weights = 1.0 - weights
+    weights = weights / np.sum(weights)
+
+    # integral = seq_len
+    weights = weights * SEQ_LEN
+    return weights
+
+def convert_to_max_minus_loss(avg_loss_per_token):
+    weights = 11.413 - avg_loss_per_token
+    weights = weights / np.sum(weights)
+    weights = weights * SEQ_LEN
+    return weights
+
+def adaptive_moving_average(data, max_window_size):
+    smoothed_data = np.zeros_like(data)
+    for i in range(len(data)):
+        window_size = min(i + 1, max_window_size)
+        smoothed_data[i] = np.mean(data[max(0, i - window_size + 1):i + 1])
+    return smoothed_data
+
+def convert_to_max_minus_loss_pow_smoothed(avg_loss_per_token, max_window_size=15):
+    # Apply adaptive moving average to smooth the data
+    avg_loss_per_token = adaptive_moving_average(avg_loss_per_token, max_window_size)
+
+    # The rest of the original function
+    weights = 11.413 - avg_loss_per_token
+    weights = weights / np.sum(weights)
+    weights = weights ** 4
+    weights = weights / np.sum(weights)
+    weights = weights * SEQ_LEN
+
+    print(np.sum(weights))
+    return weights
+
+
+if __name__ == "__main__":
+
+    compute_avg_loss_per_token()
+
+
+    '''
+    avg_loss = np.load("runs/wiki_mini/analysis/avg_loss.npy")
+    avg_loss = avg_loss[:N_RP]
+
+    # weights_base = convert_to_pdist(avg_loss)
+    weights = convert_to_max_minus_loss(avg_loss)
+    # weights = convert_to_max_minus_loss_pow_smoothed(avg_loss, max_window_size=30)
+    # plt.plot(weights_base)
+    plt.plot(weights)
+    plt.show()
+
+    print(weights.shape)
+
+    # np.save("runs/wiki_mini/analysis/weights_pow_smoothed.npy", weights)
+    '''
+
+
+
+
+
+
+
+
+
