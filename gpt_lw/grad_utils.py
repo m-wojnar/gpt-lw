f154c1d13703daec76a65e7856f9f6a4a050c75d
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-07-01 23:15:31 +0200

    Use vmap for batching

--- gpt_lw/grad_utils.py
+++ gpt_lw/grad_utils.py
@@ -6,7 +6,10 @@ def grad_norm_per_token(loss_fn, variables, key, xt, xtp1):
     params = variables.pop('params')
     state = variables
 
-    jacobian, _ = jax.jacrev(lambda p: loss_fn({'params': p, **state}, key, xt, xtp1), has_aux=True)(params)
+    jacobian = jax.vmap(
+        jax.jacrev(lambda p, x, y: loss_fn({'params': p, **state}, key, x[None], y[None])[0][0]),
+        in_axes=(None, 0, 0)
+    )(params, xt, xtp1)
     loss, _ = loss_fn({'params': params, **state}, key, xt, xtp1)
 
     grad_norms = jax.tree.map(lambda x: (x ** 2).sum(axis=tuple(range(2, x.ndim))), jacobian)
