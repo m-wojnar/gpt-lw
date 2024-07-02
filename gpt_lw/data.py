4405c7338eee008b99ca13a62ac71e8d8da41c8f
iejMac <kilianmaciej6@gmail.com>
2024-07-02 19:36:34 +0200

    inference

--- gpt_lw/data.py
+++ gpt_lw/data.py
@@ -33,15 +33,15 @@ class SimpleTokenizer(Tokenizer):
 
 class TextTokenizer(Tokenizer):
     def __init__(self):
-        # self.enc = tokenmonster.load("english-2048-clean-v1")
-        # self.enc.add_special_token("<|endoftext|>")
-        # self.vocab_size = self.enc.vocab_size
-        self.enc = tiktoken.get_encoding("cl100k_base")
-        self.vocab_size = self.enc.n_vocab
+        self.enc = tokenmonster.load("english-2048-clean-v1")
+        self.enc.add_special_token("<|endoftext|>")
+        self.vocab_size = self.enc.vocab_size
+        # self.enc = tiktoken.get_encoding("cl100k_base")
+        # self.vocab_size = self.enc.n_vocab
 
     def encode(self, text: str) -> Array:
-        # return jnp.array(self.enc.encode(text))
-        return jnp.array(self.enc.encode(text, allowed_special={'<|endoftext|>'}))
+        return jnp.array(self.enc.tokenize(text))
+        # return jnp.array(self.enc.encode(text, allowed_special={'<|endoftext|>'}))
 
     def decode(self, tokens: Array) -> str:
         return self.enc.decode(tokens.tolist())
