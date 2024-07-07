a8a3b82da223b3ad2dcc6319b7e2406e85d335c1
iejMac <kilianmaciej6@gmail.com>
2024-07-07 08:00:44 +0200

    rel weights

--- train.sh
+++ train.sh
@@ -11,7 +11,8 @@ source ${VENV_DIR}/bin/activate
 # python train.py --gpt_config configs/gpt/long_ctx.yaml --optimizer_config configs/optimizer/base.yaml --train_config configs/train/base_txt.yaml --loss_weighting runs/llama_wiki_mini/analysis/abs_weights.npy --run_name llama_wiki_mini_abs_weights
 
 # python train.py --gpt_config configs/gpt/short_ctx.yaml --optimizer_config configs/optimizer/small_lr.yaml --train_config configs/train/base_txt.yaml --loss_weighting unweighted --run_name llama_wiki_mini_short
-python train.py --gpt_config configs/gpt/short_ctx.yaml --optimizer_config configs/optimizer/small_lr.yaml --train_config configs/train/base_txt.yaml --loss_weighting runs/llama_wiki_mini/analysis/abs_weights.npy --run_name llama_wiki_mini_short_abs_w
+# python train.py --gpt_config configs/gpt/short_ctx.yaml --optimizer_config configs/optimizer/small_lr.yaml --train_config configs/train/base_txt.yaml --loss_weighting runs/llama_wiki_mini/analysis/abs_weights.npy --run_name llama_wiki_mini_short_abs_w
+python train.py --gpt_config configs/gpt/short_ctx.yaml --optimizer_config configs/optimizer/small_lr.yaml --train_config configs/train/base_txt.yaml --loss_weighting runs/llama_wiki_mini/analysis/rel_weights.npy --run_name llama_wiki_mini_short_abs_w_rel_weights
 
 # stop pod
 runpodctl stop pod $RUNPOD_POD_ID
