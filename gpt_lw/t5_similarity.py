165a8298ec90d918b4b7f53d01c407434de56c2e
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-08-23 01:01:33 +0200

    Move T5 similarity metric to gpt_lw dir

--- gpt_lw/t5_similarity.py
+++ gpt_lw/t5_similarity.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from functools import partial
 
 import jax
 import jax.numpy as jnp
@@ -6,22 +7,18 @@ import numpy as np
 from tqdm import trange
 from sentence_transformers import SentenceTransformer
 
-from gpt_lw.data import get_dataset
+from gpt_lw.data import sample_batch, get_dataset
 from gpt_lw.model_utils import load_pretrained_model, forward, init_cache
 
 
 EOT_TOKEN_NL = "<|endoftext|>"
 
 
-if __name__ == "__main__":
-    args = ArgumentParser()
-    args.add_argument("--name", type=str, default="llama_wiki_mini_short")
-    args = args.parse_args()
-
+def t5_global_similarity(name):
     batch_size = 64
     key = jax.random.PRNGKey(42)
 
-    model, variables = load_pretrained_model(f"runs/{args.name}")
+    model, variables = load_pretrained_model(f"runs/{name}")
     seq_len = model.config.seq_len
     inputs = jnp.empty((batch_size, seq_len), dtype=int)
     cache = init_cache(model, inputs)
@@ -58,5 +55,47 @@ if __name__ == "__main__":
         true_emb, gen_emb = t5_model.encode(texts), t5_model.encode(text_gen)
         cosine_sim += t5_model.similarity(true_emb, gen_emb).diag().mean().item()
 
-    cosine_sim /= n_steps
+    return cosine_sim / n_steps
+
+
+def t5_local_similarity(name):
+    batch_size = 64
+    key = jax.random.PRNGKey(42)
+
+    model, variables = load_pretrained_model(f"runs/{name}")
+    seq_len = model.config.seq_len
+    inputs = jnp.empty((batch_size, seq_len), dtype=int)
+    cache = init_cache(model, inputs)
+
+    all_tokens, tokenizer = get_dataset("text_dataset/val_wikipedia.npy", dataset_type="text")
+    gen_fn = jax.jit(lambda key, context: forward(model, variables | {'cache': cache}, key, context, method="context_gen")[0])
+    decode_fn = lambda tokens: [tokenizer.decode(t) for t in tokens]
+    t5_model = SentenceTransformer("sentence-transformers/sentence-t5-base")
+
+    sample_fn = jax.jit(partial(sample_batch, all_tokens, batch_size, seq_len + 1))
+    n_steps = 2000
+    cosine_sim = 0.0
+
+    for _ in trange(n_steps):
+        key, batch_key, model_key = jax.random.split(key, 3)
+
+        xt, _ = sample_fn(batch_key)
+        x_gen = gen_fn(model_key, xt[:, :seq_len // 2])
+
+        text_true, text_gen = decode_fn(xt[:, seq_len // 2:]), decode_fn(x_gen)
+        true_emb, gen_emb = t5_model.encode(text_true), t5_model.encode(text_gen)
+        cosine_sim += t5_model.similarity(true_emb, gen_emb).diag().mean().item()
+
+    return cosine_sim / n_steps
+
+
+if __name__ == "__main__":
+    args = ArgumentParser()
+    args.add_argument("--name", type=str, default="llama_wiki_mini_short")
+    args = args.parse_args()
+
+    cosine_sim = t5_global_similarity(args.name)
     print(f"{args.name} T5 global cosine similarity: {cosine_sim:.4f}")
+
+    cosine_sim = t5_local_similarity(args.name)
+    print(f"{args.name} T5 local cosine similarity: {cosine_sim:.4f}")
